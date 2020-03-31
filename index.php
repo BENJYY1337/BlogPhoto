637216ce1bf5a2d0973714ad099c4debcc354046
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-03-31 23:02:14 +0200

    chore: cleaned code

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-
-$file_name = "";
-$title = "";
-$text = "";
-
 //On se connecte
 $DB_NAME = "blog_photo"; //database_name
 $DB_DSN = "mysql:host=127.0.0.1:3308;dbname=" . $DB_NAME; //database_datasourcename
@@ -16,46 +11,32 @@ try {
     $query = $bdd->prepare("SELECT * FROM picture");
     $query->execute(); // Exécute une requête préparée
     $result = $query->fetchAll();
-    //var_dump($result);
 } catch (PDOException $e) {
 }
 
-
+// TODO: Check if user is logged
 ?>
-
-
-
-
 
 <!DOCTYPE html>
 <html>
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>World of Faces</title>
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
     <link rel="stylesheet" href="gallery-clean.css">
-
-
 </head>
 
 <body>
-
     <div class="container gallery-container main">
-
         <h1>World of faces</h1>
-
         <p class="page-description text-center">Chino</p>
 
         <div class="tz-gallery">
-
             <div class="row">
                 <?php
                 foreach ($result as $row) {
@@ -75,9 +56,7 @@ try {
                 }
                 ?>
             </div>
-
         </div>
-
     </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
